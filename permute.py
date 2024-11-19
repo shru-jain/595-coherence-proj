ee2deff167ae3795f6a8376379706b4ade886e37
Edwin <edwinchan129@gmail.com>
2024-11-18 23:10:32 -0500

    Fixed JSON files that contained malformed conversations

--- permute.py
+++ permute.py
@@ -12,7 +12,7 @@ def permute(json_input: Union[str, List[str]], use_separators: bool):
     for json_file in json_input:
         json_data = []
         try:
-            with open(json_file, 'r') as f:
+            with open(json_file, 'r', encoding='utf-8') as f:
                 json_data = json.load(f)
         except FileNotFoundError:
             print(f"{json_file} not found.")
@@ -20,12 +20,14 @@ def permute(json_input: Union[str, List[str]], use_separators: bool):
             print(f"{json_file} not a valid json file.")
         
         for dialogue in json_data:
+            if not dialogue['Conversation'] or not dialogue['Conversation'][0]:
+                continue
             new_conversation = {}
             # These key-value pairs can be commented out if not needed
             new_conversation['items'] = dialogue['Items']
-            new_conversation['hypothesis'] = dialogue['Hypothesis']
+            #new_conversation['hypothesis'] = dialogue['Hypothesis']
             new_conversation['original'] = dialogue['Conversation']
-            new_conversation['entailment'] = dialogue['Entailment']
+            #new_conversation['entailment'] = dialogue['Entailment']
             
             # Permutation logic
             permuted_order = list(range(1, dialogue['Items'] + 1))
@@ -50,3 +52,8 @@ def permute(json_input: Union[str, List[str]], use_separators: bool):
             new_conversation['correct_order'] = correct_order
             permuted_output.append(new_conversation)
     return permuted_output
+
+
+result = permute("datasets/no-hyp-train.json", True)
+with open("permuted-train-data.json", 'w') as f:
+    json.dump(obj=result, fp=f, indent=4)
